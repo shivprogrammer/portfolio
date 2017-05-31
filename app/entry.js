e69256d1ed5da67d931da9301697af93aa145cb6
shivprogrammer <shiv.unadkat@gmail.com>
2017-05-31 14:58:53 -0700

    more scaffolding -- entry and index files added

---
+++ app/entry.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const path = require('path');
+const angular = require('angular');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const uiRouter = require('angular-ui-router');
+const ngTouch = require('angular-touch');
+const ngAnimate = require('angular-animate');
+const uiBootstrap = require('angular-ui-bootstrap');
+
+const portfolio = angular.module('portfolio', [ngTouch, ngAnimate, uiRouter, uiBootstrap]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach( key => {
+  portfolio.config(context(key));
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  let module = context(key);
+  portfolio.controller(name, module);
+});
+
+context = require.context('./service/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  portfolio.service(name, module);
+});
+
+context = require.context('./component/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  portfolio.component(name, module);
+});
+
+context = require.context('./directive/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  portfolio.directive(name, module);
+});
